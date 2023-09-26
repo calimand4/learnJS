0e1323f3affde0fed6e59c39204e18dd37412e9c
calimandorin <calimandorin02@gmail.com>
2023-09-26 16:13:58 +0300

    format document

--- String/40.ts
+++ String/40.ts
@@ -1,14 +1,8 @@
 const string = "Un pahar cu vin !";
-
 const cuvinte = string.split(" ");
 
-cuvinte.splice(0 , 1);
-cuvinte.splice(cuvinte.length - 1 , 1);
-const response = cuvinte.join(" "); 
+cuvinte.splice(0, 1);
+cuvinte.splice(cuvinte.length - 1, 1);
+
+const response = cuvinte.join(" ");
 console.log(response);
-
-
-
-
-
-
